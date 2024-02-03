b5dbdac75af0e1ff4761a926604f414932a960d8
mac <mac@macnoMacBook-Air.local>
2024-02-03 13:52:40 +0900

    ファクトリでダミーデータを作成

---
+++ laravel-posting-app/app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $post = Auth::user()->posts()->orderBy('created_at','desc')->get();
+
+        return view('posts.index',compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show',conmpact('post'));
+    }
+}
